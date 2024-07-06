0867b00171834121a062778cb5702adf7071da18
Ali Pileforooshha <alipileforooshha@gmail.com>
2024-07-06 05:59:33 +0330

    feat(strategy) : added strategies

---
+++ app/Http/Resources/Strategy/StrategyCollection.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources\Strategy;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class StrategyCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @return array<int|string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'pagination' => [
+                'total' => $this->total(),
+                'per_page' => $this->perPage(),
+                'current_page' => $this->currentPage(),
+                'last_page' => $this->lastPage(),
+                'from' => $this->firstItem(),
+                'to' => $this->lastItem()
+            ],
+            'data' => $this->collection
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,15 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\StrategyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/get-otp',[AuthController::class, 'getOtp']);
 Route::post('/verify-otp',[AuthController::class, 'verifyOtp']);
 Route::post('/register',[AuthController::class, 'register']);
+
+Route::prefix("strategies")->middleware("auth:api")->group( function(){
+    Route::post("",[StrategyController::class, 'create'])->name("strategy.create");
+    Route::get("",[StrategyController::class, 'index'])->name("strategy.create");
+});
